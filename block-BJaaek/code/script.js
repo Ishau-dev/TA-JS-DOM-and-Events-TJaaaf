e8c420190dcd6ecfe0ffa9d4333f4d06e4dda082
Ishau <ishau@Ishaus-MacBook-Pro.local>
2021-03-18 15:14:53 -0500

    completeV1

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,10 +1,41 @@
 let first = document.querySelector('.first');
 let second = document.querySelector('.second');
 let color= ["#DFFF00","#FFBF00", "#FF7F50"];
-first.addEventListener('click',()=>{
-    first.style.backgroundColor= color[Math.floor(Math.random() * color.length)];
-});
+let generateColor= ()=>{
+    let hexchar=[
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f"
+    ];
+    let color = "#";
+    for(let i=0; i<6; i++){
+        let randomNum= Math.floor(Math.random() * hexchar.length)
+        color = color + hexchar[randomNum];
+    }
+    return color;
+}
 
-second.addEventListener('mousemove',()=>{
-    second.style.backgroundColor= color[Math.floor(Math.random() * color.length)];
-});
+let handleclick = ()=>{
+    let color = generateColor();
+    first.style.backgroundColor= color;
+}
+let handleMove = ()=>{
+    let color = generateColor();
+    second.style.backgroundColor= color;
+}
+first.addEventListener('click',handleclick);
+
+second.addEventListener('mousemove',handleMove);
