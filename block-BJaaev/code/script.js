7afb5c9bce9d1e517138c26bdb27b3d89ad6801e
Ishau <ishau@Ishaus-MacBook-Pro.local>
2021-03-27 20:20:02 -0500

    add-delete-select

--- block-BJaaev/code/script.js
+++ block-BJaaev/code/script.js
@@ -1,8 +1,14 @@
 let inputText= document.querySelector('#text');
 let root= document.querySelector('.things');
+let footer = document.querySelector('.foot');
+let count= document.querySelector('.pad');
+let activeBtn = document.querySelector('.activeBtn');
+let completed = document.querySelector('.completed');
+let all = document.querySelector('.all');
+let clear= document.querySelector('.clear');
 
 let allTodos=[];
-let count=1;
+count.innerText=0;
 let handleAdd= (event)=>{
     
     event.preventDefault();
@@ -11,40 +17,103 @@ let handleAdd= (event)=>{
         let todo= {
             name: event.target.value,
             isDone: "assets/off.svg",
-            count: count++
+            count: count.innerText++,
+            strike: ""
         };
         event.target.value="";
         allTodos.push(todo);
-        
-        createUi();
+        footer.style.display="flex";
+        createUi(allTodos);
    }
    
 }
 
-let toggleTodo = (event)=>{
+let handleDelete = (event)=>{
     let index = event.target.dataset.id;
-
-    if(allTodos[index].isDone == "assets/off.svg"){
-        allTodos[index].isDone = "assets/on.svg";
-        // allTodos[index].name = allTodos[index].name.strike();
-    //     document.querySelector('text');
-    //    ele.style.textDecoration="line-through";
-
-        createUi();
-    }else if(allTodos[index].isDone == "assets/on.svg"){
-        allTodos[index].isDone = "assets/off.svg";
-        allTodos[index].name = allTodos[index].name.strike();
-        createUi();
-    }
-    // allTodos[index].isDone=(allTodos[index].isDone)? "assets/on.svg":"assets/off.svg";
-    // createUi();
+    allTodos.splice(index,1);
+    createUi(allTodos);
 }
 
-let createUi= ()=>{
+let toggleTodo = (event)=>{
+    let index = event.target.dataset.id;
+    if(allTodos[index].isDone == "assets/off.svg"){
+        allTodos[index].isDone = "assets/on.svg";
+        allTodos[index].strike = "line-through";
+        count.innerText--;
+        createUi(allTodos);
+    }else if(allTodos[index].isDone == "assets/on.svg"){
+        allTodos[index].isDone = "assets/off.svg";
+        allTodos[index].strike = "";
+        count.innerText++;
+        createUi(allTodos);
+    }
+}
+
+let handleClear =()=>{
+    completedTodo = allTodos.filter(todo => {
+        if(todo.isDone == "assets/on.svg"){
+            return todo;   
+        }
+    });
+    completedTodo.splice(0,completedTodo.length);
+    
+    createUi(completedTodo);
+}
+clear.addEventListener('click',handleClear);
+
+ let handleAll =()=>{
+    
+     createUi(allTodos);
+ }
+ all.addEventListener('click',handleAll);
+
+let handleSelectAll = () =>{
+    
+        allTodos.forEach(todo =>{
+            if(todo.isDone == "assets/off.svg"){
+            todo.isDone = "assets/on.svg";
+            todo.strike = "line-through";
+            count.innerText--;
+            createUi(allTodos);
+            
+        }
+            else{
+                todo.isDone = "assets/off.svg";
+                todo.strike = "";
+                count.innerText++;
+                createUi(allTodos);
+            }          
+        });
+        createUi(allTodos);
+}
+let handleActive =()=>{
+   activeTodo= allTodos.filter(todo => {
+        if(todo.isDone == "assets/off.svg"){
+            return todo;   
+        }
+    });
+    createUi(activeTodo);
+}
+activeBtn.addEventListener('click',handleActive);
+
+let handleCompleted =()=>{
+    completedTodo = allTodos.filter(todo => {
+         if(todo.isDone == "assets/on.svg"){
+             
+             return todo;   
+         }
+     });
+     
+ }
+ completed.addEventListener('click',handleCompleted);
+
+
+let createUi= (arr)=>{
     root.innerHTML="";
-    allTodos.forEach((todo,index)=>{
+    arr.forEach((todo,index)=>{
         let caret = document.createElement('i');
         caret.classList.add('fas', 'search-nav', 'fa-chevron-down');
+        caret.addEventListener('click', handleSelectAll);
         let li= document.createElement('li');
         li.classList.add('flex', 'items-center', 'justify-between');
         let div = document.createElement('div');
@@ -56,39 +125,15 @@ let createUi= ()=>{
         img.addEventListener('click',toggleTodo);
         let text = document.createElement('p');
         text.innerHTML= todo.name;
-        // text.classList.add('text-dec');
+        text.style.textDecoration= todo.strike;
         let span = document.createElement('span');
         span.classList.add('close');
         span.innerText="X";
         span.setAttribute('data-id', index);
+        span.addEventListener('click',handleDelete);
         div.append(img,text);
         li.append(div,span);
-        let footer= document.createElement('li');
-        footer.classList.add('flex', 'status', 'justify-between');
-        let div2= document.createElement('div');
-        div2.classList.add('flex');
-        let span2 = document.createElement('span');
-        span2.classList.add('pad');
-        span2.innerText= todo.count;
-        let text2= document.createElement('p');
-        text2.innerText= "items left";
-        div2.append(span2,text2);
-        div3 = document.createElement('div');    
-        let button1= document.createElement('button');
-        button1.classList.add("inactive");
-        button1.innerText= "All";
-        let button2= document.createElement('button');
-        button2.innerText= "Active";
-        button2.classList.add("inactive");
-        let button3= document.createElement('button');
-        button3.innerText= "Completed";
-        button3.classList.add("inactive");
-        div3.append(button1,button2,button3);
-        let text3= document.createElement('p');
-        text3.innerText= "Clear completed";
-        text3.classList.add('pad');
-        footer.append(div2,div3,text3);
-        root.append(caret,li,footer);
+        root.append(caret,li);
     });
 }
 inputText.addEventListener('keyup',handleAdd);
